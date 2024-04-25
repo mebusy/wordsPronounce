5a69ab22f0312f1c0c846e2d01d71de0f9720fe1
mebusy <qibinyi@mebusys-iMac>
2024-04-25 10:57:49 +0800

    m

--- makeMp3DB.py
+++ makeMp3DB.py
@@ -1,37 +1,41 @@
+#!python3
 import os
-import hashlib 
+# import hashlib
 import sqlite3
 
+
 def makeDB():
-    mp3_path = "words_pronounce" 
-    if not os.path.exists( mp3_path ) :
-        return  
-    for f in os.listdir( mp3_path ) :
-        _name , _ext = os.path.splitext(f)
-        path_f = os.path.join( mp3_path, f )
-        with open( path_f ) as fp :
+    mp3_path = "words_pronounce"
+    if not os.path.exists(mp3_path):
+        return
+    for f in os.listdir(mp3_path):
+        _name, _ext = os.path.splitext(f)
+        path_f = os.path.join(mp3_path, f)
+        with open(path_f, 'rb') as fp:
             data = fp.read()
 
-            c.execute("INSERT or replace INTO pron VALUES ( ?,?)" , ( _name .decode( "utf8" ) , buffer(data) ) )
+            c.execute("INSERT or replace INTO pron VALUES ( ?,?)",
+                      (_name, data))
     pass
-                     
 
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
+    db_file = "pron.db"
+    # if exists, remove
+    if os.path.exists(db_file):
+        os.remove(db_file)
 
-    db_file = "pron.db" 
     conn = sqlite3.connect(db_file)
     c = conn.cursor()
-        
 
-    c.execute( "drop table if exists pron "  )
-    c.execute( '''create table if not exists pron
-                    ( word text primary key ,  voice BLOB NOT NULL  ) ''' )
+    c.execute("drop table if exists pron ")
+    c.execute('''create table if not exists pron
+                    ( word text primary key ,  voice BLOB NOT NULL  ) ''')
 
     makeDB()
+    conn.commit()
 
-    # c.execute( '''create UNIQUE index index_pron_word_voice on pron ( word , voice  ) ''' )
-    c.execute( "vacuum" )
+    c.execute("vacuum")
 
     conn.commit()
     conn.close()
